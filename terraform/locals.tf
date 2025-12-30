13940eec111ca442f36aaaa1c446dcef74639465
Mohamed <mohamed0395@gmail.com>
2025-12-30 10:01:24 -0500

    Provide Terraform inputs in CI and run Checkov via pip

---
+++ terraform/locals.tf
@@ -0,0 +1,25 @@
+locals {
+  mandatory_tag_keys = [
+    "Project",
+    "Owner",
+    "Env",
+    "CostCenter",
+    "TTL",
+  ]
+
+  mandatory_tags = merge(
+    var.extra_tags,
+    {
+      Project    = var.project
+      Owner      = var.owner
+      Env        = lower(var.env)
+      CostCenter = var.cost_center
+      TTL        = var.ttl
+    }
+  )
+
+  missing_or_empty_mandatory_tags = [
+    for key in local.mandatory_tag_keys : key
+    if !contains(local.mandatory_tags, key) || length(trimspace(local.mandatory_tags[key])) == 0
+  ]
+}
